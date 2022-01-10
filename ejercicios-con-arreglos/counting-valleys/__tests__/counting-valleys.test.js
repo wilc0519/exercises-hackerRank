e0564ac735916fcf328a0c8ea4fb9b2389f06db0
wilc0519 <wilc0519@gmail.com>
2022-01-10 09:58:43 -0500

    counting valleys

---
+++ ejercicios-con-arreglos/counting-valleys/__tests__/counting-valleys.test.js
@@ -0,0 +1,12 @@
+const findNumberOfValleys = require("../couting-valleys")
+
+test(`Given the sequence of up and down steps during a hike, U represents one step up
+ and D represents one step down, a valley starting with one step down from sea level 
+ and ending with one step up from sea level, find and print the number of valleys 
+ walked through.`, () => {
+    const steps = 8
+    const path = 'UDDDUDUU'
+
+    const numberOfValleys = findNumberOfValleys(steps, path)
+    expect(numberOfValleys).toEqual(1)
+})
